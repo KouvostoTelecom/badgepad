69d0ce7f1fca5c3e48fefb4e626a2b0295d27ee5
Joni Hasanen <joni@pieceofcode.com>
2020-11-09 21:32:08 +0200

    Generate keymaps

--- src/ducky_convert.py
+++ src/ducky_convert.py
@@ -5,6 +5,8 @@ import argparse
 import quantum
 
 class DuckyConvert:
+    DEFAULT_KEY = "ANY_KEY"
+
     def __init__(self, scripts_dir):
         self.scripts_dir = scripts_dir
 
@@ -12,6 +14,8 @@ class DuckyConvert:
         converted = self.convert_scripts()
         keycode_enums = self.generate_enum_keycodes(converted)
         print(keycode_enums)
+        keymap = self.generate_keymap(converted)
+        print(keymap)
 
     def convert_scripts(self):
         scripts = {}
@@ -27,16 +31,49 @@ class DuckyConvert:
         return scripts
 
     def generate_enum_keycodes(self, scripts_dict):
-        # enum custom_keycodes {
-        #                 NC
-        #             };
         output = ["enum custom_keycodes {"]
+        output.append(self.DEFAULT_KEY)
         for layerkey in sorted(scripts_dict.keys()):
             for scriptskey in sorted(scripts_dict[layerkey].keys()):
-                output.append("_".join([layerkey, scriptskey]))
+                output.append(self.keycode(layerkey, scriptskey))
         output.append("}")
 
         return output
+
+    def generate_keymap(self, scripts_dict):
+        output = ["const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {"]
+        for layer_index, layerkey in enumerate(sorted(scripts_dict.keys())):
+            keys = sorted(scripts_dict[layerkey].keys())[:8]
+            num_of_scripts  = len(keys)
+
+            # for script_index, scriptskey in enumerate(sorted(scripts_dict[layerkey].keys())):
+                       # jkk[1] = LAYOUT(
+                  # ),
+            output.append("[{}]  = LAYOUT(".format(layer_index))
+            output.append("""
+                         {},             {},
+                   {},       {}, {},       {},
+                         {},             {}
+                          """.format(
+                              self.keycode(layerkey, keys[0]) if 0 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[1]) if 1 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[2]) if 2 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[3]) if 3 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[4]) if 4 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[5]) if 5 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[6]) if 6 < num_of_scripts else self.DEFAULT_KEY,
+                              self.keycode(layerkey, keys[7]) if 7 < num_of_scripts else self.DEFAULT_KEY
+                          ))
+            output.append("),")
+        output.append("}")
+
+        return output
+
+    def keycode(self , layerkey, scriptskey):
+        return "_".join([layerkey, scriptskey])
+
+
+
 
 
 
