0652b4c4bc3907a5ef5949db165f2b901a298f80
Joni Hasanen <joni@pieceofcode.com>
2020-11-15 14:12:45 +0200

    Update

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-#TODO: convert ducky scripts to c code
 TARGET_FILE=/app/qmk_firmware/keyboards/disobey2018/shared_keyboard_utils.c
 python3 ducky_convert.py --scripts /app/scripts/ --target $TARGET_FILE --lang fi
-cat $TARGET_FILE
 qmk compile --keyboard disobey2018 --keymap default
 mv /app/qmk_firmware/*.bin /build/
