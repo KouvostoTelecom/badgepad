b07c65cf87a742532c1b3166a7e9f01ba9ad5e32
Joni Hasanen <joni@pieceofcode.com>
2020-11-15 20:52:09 +0200

    Update compile script
    Fail early

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 TARGET_FILE=/app/qmk_firmware/keyboards/disobey2018/shared_keyboard_utils.c
-python3 ducky_convert.py --scripts /app/scripts/ --target $TARGET_FILE --lang fi
-qmk compile --keyboard disobey2018 --keymap default
+python3 ducky_convert.py --scripts /app/scripts/ --target $TARGET_FILE --lang fi && \
+qmk compile --keyboard disobey2018 --keymap default && \
 mv /app/qmk_firmware/*.bin /build/
